99cae2a0670ced6ddb2ace0c2359d1fff08a7398
quechivvt <chi.nguyen2407k22@hcmut.edu.vn>
2024-04-20 23:39:40 +0700

    MedicalForm, Patient (1st edition)
    1/Create controller, repository of Patient and Medical Form
    2/add new request method controller of Sickness.

--- SpringBootAPI_HospitalManagement/src/main/java/com/chipichapa/hospital/patient/model/Patient.java
+++ SpringBootAPI_HospitalManagement/src/main/java/com/chipichapa/hospital/patient/model/Patient.java
@@ -1,6 +1,7 @@
 package com.chipichapa.hospital.patient.model;
 
 import com.chipichapa.hospital.medicalForm.MedicalForm;
+import com.chipichapa.hospital.sickness.Sickness;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
@@ -32,5 +33,75 @@ public class Patient {
     @JoinColumn(name = "user_id")
     private List<MedicalForm> medicalForms;
 
+    //Constructor
+    public Patient(){}
+    public Patient(long id, String fullName, String address, LocalDate dateOfBirth, String phoneNumber, String gender, List<MedicalForm> medicalForms) {
+        this.id = id;
+        this.fullName = fullName;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+        this.medicalForms = medicalForms;
+    }
+
+    //getter and setter
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public List<MedicalForm> getMedicalForms() {
+        return medicalForms;
+    }
+
+    public void setMedicalForms(List<MedicalForm> medicalForms) {
+        this.medicalForms = medicalForms;
+    }
+
+    //other method
 
 }
--- SpringBootAPI_HospitalManagement/src/main/java/com/chipichapa/hospital/sickness/SicknessController.java
+++ SpringBootAPI_HospitalManagement/src/main/java/com/chipichapa/hospital/sickness/SicknessController.java
@@ -26,8 +26,8 @@ public class SicknessController
     private SicknessRepository sicknessRepository;
 
     //get request methods
-    @GetMapping("/dnoomnse/sickness/getAllSickness")
-    public List<Sickness> getAllSickness()
+    @GetMapping("/dnoomnse/sickness/getAllSicknesses")
+    public List<Sickness> getAllSicknesses()
     {
         return  sicknessRepository.findAll();
     }
@@ -49,17 +49,30 @@ public class SicknessController
     }
 
     //post request methods
-    @PostMapping("/dnoomse/sickness/post")
-    public Sickness sickness(@RequestBody Sickness sickness)
+    @PostMapping("/dnoomse/sickness/postSickness")
+    public Sickness postSickness(@RequestBody Sickness sickness)
     {
         Standardization standardization = new Standardization();
         sickness.setSickName(standardization.StandardSpace(sickness.getSickName()));
         return sicknessRepository.save(sickness);
     }
 
+    @PostMapping("/dnoomse/sickness/postListSicknesses")
+    public List<Sickness>  postListSicknesses(@RequestBody List<Sickness> sicknesses)
+    {
+        Standardization standardization = new Standardization();
+        int num = sicknesses.size();
+        for (int i = 0; i < num ; i++)
+        {
+            sicknesses.get(i).setSickName(standardization.StandardSpace(sicknesses.get(i).getSickName()));
+        }
+        return sicknessRepository.saveAll(sicknesses);
+
+    }
+
     //put request methods
-    @PutMapping("/dnoomse/sickness/putThroughID/{id}")
-    public ResponseEntity<Sickness> putThroughID (@PathVariable(name = "id") Long id, @RequestBody Sickness newSickness)
+    @PutMapping("/dnoomse/sickness/putSicknessThroughID/{id}")
+    public ResponseEntity<Sickness> putSicknessThroughID (@PathVariable(name = "id") Long id, @RequestBody Sickness newSickness)
     {
         Sickness sickness = sicknessRepository.findById(id)
                 .orElseThrow(()-> new Exception("Sickness with id" + id + "not exist"));
@@ -69,12 +82,21 @@ public class SicknessController
     }
 
     //delete request methods
-    @DeleteMapping("/dnoomse/sickness/deleteThroughID/{id}")
+    @DeleteMapping("/dnoomse/sickness/deleteSicknessThroughID/{id}")
     public ResponseEntity<Map<String,Boolean>> deleteThroughID (@PathVariable(name = "id") Long id)
     {
         Sickness sickness = sicknessRepository.findById(id)
                 .orElseThrow(()-> new Exception("Sickness with id" + id + "not exist"));
         sicknessRepository.delete(sickness);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/dnoomse/sickness/deleteAllSickness")
+    public ResponseEntity<Map<String,Boolean>> deleteAllSickness()
+    {
+        sicknessRepository.deleteAll(sicknessRepository.findAll());
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
         return ResponseEntity.ok(response);
